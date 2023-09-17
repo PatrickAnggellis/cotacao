d2f7fd1b1233bed238f5392ee48435321d119787
Patrick D' Anggellis <patrickbananeiras@gmail.com>
2023-09-16 23:31:32 -0300

    First commit and inclusion of dto and entity

---
+++ src/main/java/org/br/mineradora/dto/USDBRL.java
@@ -0,0 +1,13 @@
+package org.br.mineradora.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
+
+@Jacksonized
+@Data
+@Builder
+@AllArgsConstructor
+public class USDBRL {
+}
