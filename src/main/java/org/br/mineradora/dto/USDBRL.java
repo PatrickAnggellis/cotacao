17bb0143a691d98018df40d947f88e20acaf97f5
Patrick D' Anggellis <patrickbananeiras@gmail.com>
2023-09-22 23:57:32 -0300

    Inserção do service, kafkaEvent e ajuste na classe USDBRL

--- src/main/java/org/br/mineradora/dto/USDBRL.java
+++ src/main/java/org/br/mineradora/dto/USDBRL.java
@@ -10,4 +10,19 @@ import lombok.extern.jackson.Jacksonized;
 @Builder
 @AllArgsConstructor
 public class USDBRL {
+
+    public String code;
+    public String codein;
+    public String name;
+    public String high;
+    public String low;
+    public String varBid;
+    public String pctChange;
+    public String bid;
+    public String ask;
+    public String timestamp;
+    public String create_data;
+
+
+
 }
---
+++ src/main/java/org/br/mineradora/service/QuotationService.java
@@ -0,0 +1,79 @@
+package org.br.mineradora.service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.br.mineradora.client.CurrencyPriceClient;
+import org.br.mineradora.dto.CurrencyPriceDTO;
+import org.br.mineradora.dto.QuotationDTO;
+import org.br.mineradora.entity.QuotationEntity;
+import org.br.mineradora.message.KafkaEvents;
+import org.br.mineradora.repository.QuotationRepository;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+@ApplicationScoped
+public record QuotationService() {
+
+    @Inject
+    @RestClient
+    static CurrencyPriceClient currencyPriceClient;
+
+    @Inject
+    static QuotationRepository quotationRepository;
+
+    @Inject
+    static KafkaEvents kafkaEvents;
+
+
+    public void getCurrencyPrice(){
+
+        CurrencyPriceDTO currencyPriceInfo = currencyPriceClient.getPriceByPair("USD-BRL");
+
+        if(updateCurrencyPriceInfo(currencyPriceInfo)){
+            kafkaEvents.sendNewKafkaEvent(QuotationDTO
+                    .builder()
+                    .currencyPrice(new BigDecimal(currencyPriceInfo.getUSDBRL().getBid()))
+                    .date(new Date())
+                    .build());
+        }
+
+    }
+
+    private boolean updateCurrencyPriceInfo(CurrencyPriceDTO currencyPriceInfo) {
+
+        BigDecimal currentPrice = new BigDecimal(currencyPriceInfo.getUSDBRL().getBid());
+        boolean updatePrice = false;
+
+        List<QuotationEntity> quotationEntityList = quotationRepository.findAll().list();
+
+        if(quotationEntityList.isEmpty()){
+            saveQuotation(currencyPriceInfo);
+            updatePrice = true;
+        } else {
+            QuotationEntity lastDollarPrice = quotationEntityList
+                    .get(quotationEntityList.size() - 1);
+
+            if(currentPrice.floatValue() > lastDollarPrice.getCurrencyPrice().floatValue()){
+                updatePrice = true;
+                saveQuotation(currencyPriceInfo);
+            }
+        }
+        return updatePrice;
+    }
+
+    private void saveQuotation(CurrencyPriceDTO currencyPriceInfo) {
+
+        QuotationEntity quotation = new QuotationEntity();
+
+        quotation.setDate(new Date());
+        quotation.setPair("USD-BRL");
+        quotation.setCurrencyPrice(new BigDecimal(currencyPriceInfo.getUSDBRL().getBid()));
+        quotation.setPctChange(currencyPriceInfo.getUSDBRL().pctChange);
+
+        quotationRepository.persist(quotation);
+
+    }
+}
